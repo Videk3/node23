629bc09921a3b6cc3554a325623286b9b7d30325
Videk3 <vid.berlocnik2@scv.si>
2023-05-05 09:16:03 +0200

    Auth local and jwt changes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,29 @@
 import { Controller, Post, UseGuards, Request, Get, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { AuthGuard } from "@nestjs/passport";
 import { Response } from "express";
+import { LocalAuthGuard } from "./guards/localAuth.guard";
+import { JwtAuthGuard } from "./guards/jwtAuth.guard";
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {
   }
 
-  @UseGuards(AuthGuard('local'))
-  @Post('signin')
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
   signIn(@Request() req, @Res() res: Response){
     const jwt = req.user;
     res.setHeader('Set-Cookie', [jwt]).json();
-    return req.user;
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get('profile')
   profile(@Request() req){
     return req.user;
+  }
+
+  @Post('logout')
+  logout(@Request() req){
+    req.res.setHeader("Set-Cookie", `Access_token=; HttpOnly; Path=/; Max-Age=0`);
   }
 }
